22cecdd4873550534322f5a1f7ee22e5867fe6e8
Junichi Kakisako <kilo.sw@gmail.com>
2014-12-10 21:07:25 +0900

    added models

--- potte/models.py
+++ potte/models.py
@@ -1,16 +1,21 @@
+# coding: utf-8
+
 from sqlalchemy import (
+    Table,
     Column,
-    Index,
+    ForeignKey,
     Integer,
-    Text,
-    )
+    Unicode,
+    DateTime,
+)
 
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
-    )
+    relationship,
+)
 
 from zope.sqlalchemy import ZopeTransactionExtension
 
@@ -18,10 +23,52 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+class User(Base):
+    __tablename__ = 'user'
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+    username = Column(Unicode(256), primary_key=True)
+    password = Column(Unicode(256), nullable=False)
+    email = Column(Unicode(256), nullable=False)
+
+    albums = relationship("Album", backref="user")
+    photos = relationship("Photo", backref="user")
+
+
+class Album(Base):
+    __tablename__ = 'album'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(Unicode(256), ForeignKey("user.username"))
+
+    name = Column(Unicode(256), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+    photos = relationship("Photo", secondary="album_has_photo",
+                          backref="albums")
+
+
+class Photo(Base):
+    __tablename__ = 'photo'
+
+    id = Column(Unicode(256), primary_key=True)
+    username = Column(Unicode(256), ForeignKey("user.username"))
+
+    created_at = Column(DateTime, nullable=False)
+
+    thumbnails = relationship("PhotoThumbnail", backref="photo")
+
+
+class PhotoThumbnail(Base):
+    __tablename__ = 'photo_thumbnail'
+
+    id = Column(Unicode(256), primary_key=True)
+    photo_id = Column(Unicode(256), ForeignKey('photo.id'))
+
+    size = Column(Unicode(16))
+
+
+album_has_photo = Table(
+    'album_has_photo', Base.metadata,
+    Column('album_id', Integer, ForeignKey('album.id')),
+    Column('photo_id', Unicode(256), ForeignKey('photo.id')),
+)
